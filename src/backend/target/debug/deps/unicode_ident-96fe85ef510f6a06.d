e2a6b3fab050021c98bd5f524033878e49eb8da4
dperry713 <dperry713@gmail.com>
2026-06-30 19:05:20 -0400

    2

---
+++ src/backend/target/debug/deps/unicode_ident-96fe85ef510f6a06.d
@@ -0,0 +1,8 @@
+C:\Users\d\Agent_OS\src\backend\target\debug\deps\unicode_ident-96fe85ef510f6a06.d: C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
+
+C:\Users\d\Agent_OS\src\backend\target\debug\deps\libunicode_ident-96fe85ef510f6a06.rlib: C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
+
+C:\Users\d\Agent_OS\src\backend\target\debug\deps\libunicode_ident-96fe85ef510f6a06.rmeta: C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs
+
+C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\lib.rs:
+C:\Users\d\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\unicode-ident-1.0.24\src\tables.rs:
